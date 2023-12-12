46079ffaae6b394ceb9fc8db5f23d38131636a14
Newton-Bundi <newton.bundi@student.moringaschool.com>
2023-12-12 22:41:51 +0300

    Populated navbar and renamed components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Home from "./home"
-import AvailablePets from "./AvailablePets";
-import SignupForm from "./SignupForm";
-import Owners from './Owners';
-import Contact from './Contact';
-import About from './About';
-import NavBar from './Nav';
+import AvailablePets from "./availablepets";
+import SignupForm from "./signupform";
+import Owners from './owners';
+import Contact from './contact';
+import About from './about';
+import NavBar from './nav';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
