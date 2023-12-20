ca043e776a6607bd9476a0e21e69b5d00e471b60
Newton-Bundi <newton.bundi@student.moringaschool.com>
2023-12-20 20:54:03 +0300

    Updated contact page

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -9,7 +9,7 @@ function Contact() {
             <p>Have any inquiries, suggestions, concerns, or feedback?</p>
             <p>We'd love to hear from you.</p>
             <div className='contacts'>
-                <h4>Emails</h4>
+                <h1>Emails</h1>
                 <p>John Makateto: john.makateto@student.moringaschool.com</p>
                 <p>Peris Kingori :periskingori@gmail.com</p>
                 <p>Eunice Mwenda: eunice.mwenda@student.moringaschool.com</p>
